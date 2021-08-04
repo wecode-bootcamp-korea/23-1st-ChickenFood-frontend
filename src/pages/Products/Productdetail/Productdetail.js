54d2a1d28f59e3b67db341e736abc12dcd62484d
Lown Kim <wideplate3@gmail.com>
2021-08-04 20:42:10 +0900

    ADD: Product Detail 레이아웃 구현

--- src/pages/Products/Productdetail/Productdetail.js
+++ src/pages/Products/Productdetail/Productdetail.js
@@ -11,7 +11,7 @@ class Productdetail extends React.Component {
             <img
               className="sampleImage"
               alt="product"
-              src="./images/sample.jpeg"
+              src="./images/sheabutter_cream.png"
             />
           </div>
         </section>
@@ -21,9 +21,11 @@ class Productdetail extends React.Component {
             {/* 섹션1 */}
             <div className="sectionOne">
               <div className="productName">
-                수비드 마스크 시트 3+3
-                <i className="far fa-heart"></i>
-                <i className="fas fa-share-alt"></i>
+                수비드 마스크 시트
+                <div className="iconsWrapper">
+                  <img className="heart" alt="heart" src="./images/heart.png" />
+                  <img className="share" alt="share" src="./images/share.png" />
+                </div>
               </div>
               <div className="productPrice">￦1,100</div>
             </div>
@@ -33,35 +35,42 @@ class Productdetail extends React.Component {
                 <span className="contentsTitle">쿠폰</span>
                 <button className="couponBtn">
                   멤버십 상시 10% 할인(110할인)
-                  <i class="fas fa-arrow-down"></i>
+                  <img
+                    className="download"
+                    alt="download"
+                    src="./images/down-arrow.png"
+                  />
                 </button>
               </div>
               <div className="tags">
                 <span className="contentsTitle">관련태그</span>
                 <span className="tagsWrapper">
-                  <button>#비건</button>
-                  <button>#데일리템</button>
-                  <button>#생기맑음</button>
+                  <span>#비건</span>
+                  <span>#데일리템</span>
+                  <span>#생기맑음</span>
+                  <span>#촉촉</span>
                 </span>
               </div>
               <div className="shipping">
                 <span className="contentsTitle">배송비</span>
-                <span>2,750원(20,000원 이상 결제시 무료배송)</span>
+                <span className="shippingFee">
+                  2,750원(20,000원 이상 결제시 무료배송)
+                </span>
               </div>
               <div className="review">
                 <span className="contentsTitle">리뷰</span>
-                <span>★★★★★ (3건)</span>
+                <span className="start">★★★★★ (3건)</span>
               </div>
             </div>
             {/* 섹션3 */}
-            <div className="sectionThird">
+            <div className="sectionThree">
               <div className="options"></div>
             </div>
             {/* 섹션4 */}
-            <div className="sectionFourth">
+            <div className="sectionFour">
               <div className="selectedProduct">
                 <div>
-                  <span className="contentsTitle">토마토</span>
+                  <span className="contentsTitle title">토마토</span>
                   <i class="fas fa-times-circle"></i>
                 </div>
                 <div>
@@ -69,7 +78,8 @@ class Productdetail extends React.Component {
                 </div>
               </div>
             </div>
-            <div className="sectionFifth">
+            {/* 섹션5 */}
+            <div className="sectionFive">
               <div className="productTotal">
                 <span className="contentsTitle">합계</span>
                 <span>수량 : 2</span>

0afc5a9789b65f612abbbda11da00e85247d7641
Lown Kim <wideplate3@gmail.com>
2021-08-07 14:43:57 +0900

    ADD: Product Detail 레이아웃 구현 완료

--- src/pages/Products/Productdetail/Productdetail.js
+++ src/pages/Products/Productdetail/Productdetail.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Productdetails.scss';
 
+// 금액 합계 로직 대애충
+// 1. 빈 배열 생성
+// 2. 셀렉트박스에서 옵션 선택(클릭)할 때
+// 3. data.price의 금액을 빈 배열로 가져오삼
+// 4. 그리고나서 합계 란에 그 배열 값 띄우기
+// 4. 다른 옵션 선택 할 때마다 data.price금액이 계속 더해짐
+// 배열? number니까 스트링은 안되고.. 배열에 넣는게 맞나?
+
 class Productdetail extends React.Component {
   constructor() {
     super();
     this.state = {
       data: {},
+      emptyList: [],
     };
   }
 
@@ -15,14 +24,20 @@ class Productdetail extends React.Component {
     })
       .then(res => res.json())
       .then(data => {
-        this.setState({ data: data.result });
+        this.setState({ data: data.items });
       });
   }
 
+  test = e => {
+    console.log(123);
+    console.log(e.target.value);
+    this.setState({ emptyList: this.state.emptyList.concat(e.target.value) });
+  };
+
   render() {
-    const { data } = this.state;
-    // const data = this.state.data;
-    console.log(this.state.data);
+    // console.log('스테이트 데이터', this.state.data.name);
+    console.log(this.state.emptyList);
+    const data = this.state.data;
     return (
       <div className="productDetail">
         {/* 왼쪽 */}
@@ -47,7 +62,9 @@ class Productdetail extends React.Component {
                   <img className="share" alt="share" src="./images/share.png" />
                 </div>
               </div>
-              <div className="productPrice">₩{data.price}</div>
+              <div className="productPrice">
+                ₩{Math.floor(data.price).toLocaleString('ko-KR')}
+              </div>
             </div>
             {/* 섹션2 */}
             <div className="sectionTwo">
@@ -85,40 +102,54 @@ class Productdetail extends React.Component {
             </div>
             {/* 섹션3 */}
             <div className="sectionThree">
-              <div className="sectionFour">
-                <div className="selectedProduct">
-                  <div>
-                    <div className="contentsTitle title">{data.name}</div>
-                    <div></div>
-                  </div>
-                  <div>
-                    <div className="productQuantity">
-                      <span className="plusAndMinusWrapper">
-                        <img
-                          className="plusAndMinus"
-                          src="./images/Minus-sign-in-a-circle.svg.png"
-                        />
-                        <span className="quantity">1</span>
-                        <img
-                          className="plusAndMinus"
-                          src="./images/plus_in_circle.png"
-                        />
-                      </span>
-                      <span className="plusAndMinusPrice">{data.price}</span>
-                    </div>
-                  </div>
+              <div>
+                <div className="optionWrapper">
+                  <select className="selectBox" onChange={this.test}>
+                    <option value="옵션선택">옵션을 선택해주세요</option>
+                    <option value="옵션1">{data.type_name}</option>
+                    <option value="옵션2">{data.type_name}</option>
+                    <option value="옵션3">{data.type_name}</option>
+                    <option value="옵션4">{data.type_name}</option>
+                  </select>
                 </div>
               </div>
+              <div>
+                <div className="productQuantity"> </div>
+              </div>
             </div>
+            <div></div>
             {/* 섹션4 */}
             <div className="sectionFour">
+              <div className="selectedProduct">
+                <div className="selectedProductName">
+                  <span className="contentsTitle">석류향</span>
+                  <img
+                    className="plusAndMinus close"
+                    src="./images/close-cross-symbol-in-a-circle.png"
+                  />
+                </div>
+
+                <div className="plusAndMinusWrapper">
+                  <span className="plusAndMinus">
+                    <img className="plusAndMinus" src="./images/minus.png" />
+                    <span className="quantity">1</span>
+                    <img className="plusAndMinus" src="./images/plus.png" />
+                  </span>
+
+                  <span>
+                    <span className="plusAndMinusPrice">
+                      ₩{Math.floor(data.price).toLocaleString('ko-KR')}
+                    </span>
+                  </span>
+                </div>
+              </div>
               <div className="productTotal">
                 <span className="quantityWrapper">
                   <span className="contentsTitle">합계</span>
                   <span>수량</span>
-                  <span> 2</span>
+                  <span>0</span>
                 </span>
-                <span className="totalAmount">₩2,200</span>
+                <span className="totalAmount">₩0</span>
               </div>
               <div className="BtnsWrapper">
                 <button className="putInCart">장바구니 담기</button>

4b684604b0f77f53d13f9b5e23a600ada532336f
JaeHyun <nam2350@gmail.com>
2021-08-10 14:02:38 +0900

    Add: 위시리스레이아웃

--- src/components/Wishlist/Wishlist.js
+++ src/components/Wishlist/Wishlist.js
@@ -10,11 +10,19 @@ class Wishlist extends React.Component {
       <div className="wishlistWraper">
         <h1 className="wishlistTitle">위시리스트</h1>
         <div className="wishlistContainer">
-          <img src="./images/icon_nowish.png" alt="nowish" />
+          <img
+            className="nowishimg"
+            src="./images/icon_nowish.png"
+            alt="nowish"
+          />
           <div className="nowishText">
-            <p>위시리스트에 담긴 상품이 없습니다.</p>
-            <p>마음에 드는 상품은 하트를</p>
-            <p>꾹~ 눌러 위시리스트에 보관할 수 있습니다:)</p>
+            <p className="emptyContent title">
+              위시리스트에 담긴 상품이 없습니다.
+            </p>
+            <p className="emptyContent">마음에 드는 상품은 하트를</p>
+            <p className="emptyContent">
+              꾹~ 눌러 위시리스트에 보관할 수 있습니다:)
+            </p>
           </div>
         </div>
       </div>

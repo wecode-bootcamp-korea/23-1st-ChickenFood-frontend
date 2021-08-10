32d56c81122e5a287ce4e41e683cf5437ed03104
택준 <taekjunsim@gmail.com>
2021-08-10 11:43:42 +0900

    "Modify: 성훈님 리뷰 반영" - main.js 삭제 - ListComponent fragment 삭제 - ListComponent 구조분해할당 - Routes.js 수정

--- src/Routes.js
+++ src/Routes.js
@@ -2,21 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Nav from './components/Nav/Nav';
-import Main from './pages/Main/Main';
-import Products from './pages/Products/Products';
-import Productdetail from './pages/Products/Productdetail/Productdetail';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Nav />
-        <Switch>
-          <Route exact path="/" component={Main} />
-          {/* <Route exact path="/header" component={Header} /> */}
-          <Route exact path="/products" component={Products} />
-          <Route exact path="/productdetail" component={Productdetail} />
-        </Switch>
       </Router>
     );
   }

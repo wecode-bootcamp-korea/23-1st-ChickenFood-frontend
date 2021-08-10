939b61eb55c847333f9a8a05875bfcd56e4d03bf
택준 <taekjunsim@gmail.com>
2021-08-10 20:30:10 +0900

    삭제 예정 커밋

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Header from './pages/Header/Header';
-import Main from './pages/Main/Main';
-import Products from './pages/Products/Products';
-import Productdetail from './pages/Products/Productdetail/Productdetail';
+import SignIn from './pages/Login/SignIn';
+import Nav from './components/Nav/Nav';
+// import Products from './pages/Products/Products';
+// import Productdetail from './pages/Products/Productdetail/Productdetail';
 import Footer from './components/Footer/Footer';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
-        {/* <Nav /> */}
+        <Nav />
         <Switch>
-          {/* <Route exact path="/signin" component={SignIn} /> */}
-          <Route exact path="/" component={Main} />
-          <Route exact path="/products" component={Products} />
-          <Route exact path="/productdetail" component={Productdetail} />
+          <Route exact path="/" component={SignIn} />
+          {/* <Route exact path="/" component={Main} /> */}
+          {/* <Route exact path="/products" component={Products} /> */}
+          {/* <Route exact path="/productdetail" component={Productdetail} /> */}
         </Switch>
-        {/* <Footer /> */}
+        <Footer />
       </Router>
     );
   }

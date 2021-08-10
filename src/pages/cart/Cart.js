7df0c905124f1557d80d95e866a5b0eeca605234
mekemeke94 <thth3194@gmail.com>
2021-08-10 12:59:06 +0900

    Add: 장바구니 페이지 버튼 클릭 시 개수 추가, 개수 추가에 따른 금액 변화 기능 구현(총 상품금액 미포함)

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -8,6 +8,7 @@ class Cart extends React.Component {
 
     this.state = {
       items: [],
+      totalPrice: 0,
     };
   }
 
@@ -16,13 +17,56 @@ class Cart extends React.Component {
       .then(res => res.json())
       .then(res => {
         this.setState({
-          items: res.item,
+          items: res.items,
         });
       });
   }
 
-  render() {
+  plusBtn = e => {
     const { items } = this.state;
+    const changeList = [...items];
+    changeList.map(el => {
+      if (Number(e.target.name) === el.id) {
+        el.quantity = el.quantity + 1;
+      }
+    });
+    this.setState({
+      items: changeList,
+    });
+  };
+
+  minusBtn = e => {
+    const { items } = this.state;
+    const changeList = [...items];
+    changeList.map(el => {
+      if (Number(e.target.name) === el.id) {
+        el.quantity = el.quantity - 1;
+        if (el.quantity < 1) {
+          el.quantity = 1;
+        }
+      }
+    });
+    this.setState({
+      items: changeList,
+    });
+  };
+
+  test = data => {
+    let result = '';
+    result = data;
+    console.log('totalPrice=', result);
+    this.setState({
+      totalPrice: result,
+    });
+  };
+
+  render() {
+    const { items, totalPrice } = this.state;
+    const { plusBtn, minusBtn } = this;
+    let shippingFee = 2500;
+    if (totalPrice > 20000 || totalPrice === 0) {
+      shippingFee = 0;
+    }
     return (
       <div className="cartContainer">
         <div className="cartLayout">
@@ -38,7 +82,7 @@ class Cart extends React.Component {
                       <input
                         type="checkbox"
                         className="checkHide"
-                        checked="checked"
+                        // onClick={this.test}
                       ></input>
                     </div>
                     <div>전체 선택</div>
@@ -47,10 +91,15 @@ class Cart extends React.Component {
                 </div>
                 {items.map(el => (
                   <OrderItems
-                    key={el.id}
-                    itemName={el.itemName}
-                    itemImg={el.itemImg}
-                    itemPrice={el.itemPrice}
+                    id={el.id}
+                    itemName={el.name}
+                    itemImg={el.thumbnail}
+                    itemPrice={el.price}
+                    itemNum={el.quantity}
+                    plusBtn={plusBtn}
+                    minusBtn={minusBtn}
+                    total={el.price * el.quantity}
+                    test={this.test}
                   />
                 ))}
               </div>
@@ -62,13 +111,13 @@ class Cart extends React.Component {
                   <div className="billContentsTitle">일반 상품</div>
                   <div className="price item">
                     <div>총 상품 금액</div>
-                    <div className="priceNum">\0</div>
+                    <div className="priceNum">\{totalPrice}</div>
                   </div>
                   <div className="price shipping">
                     <div>
                       배송비 <span>(20,000원 이상 결제시 무료배송)</span>
                     </div>
-                    <div className="priceNum">(+) \0</div>
+                    <div className="priceNum">(+) \{shippingFee}</div>
                   </div>
                   <div className="price discount">
                     <div>할인 금액</div>
@@ -77,7 +126,9 @@ class Cart extends React.Component {
                 </div>
                 <div className="price total">
                   <div>결제 예정 금액</div>
-                  <div className="priceNum">\0</div>
+                  <div className="priceNum">
+                    \{Number(totalPrice) + shippingFee}
+                  </div>
                 </div>
               </div>
               <div className="buyBtnBox">
--- src/pages/cart/OrderItems/OrderItems.js
+++ src/pages/cart/OrderItems/OrderItems.js
@@ -2,23 +2,71 @@ import React from 'react';
 import './OrderItems.scss';
 
 class OrderItems extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      totalPrice: '',
+      cart: [],
+    };
+  }
+  // getCheckboxValue = e => {
+  //   const { cart } = this.state;
+  //   let totalCart = cart.push(this.props.total);
+  //   console.log('setstate전:', cart);
+  //   this.setState({
+  //     cart: totalCart,
+  //   });
+  //   console.log('setstate 후:', cart);
+  //   if (e.target.checked) {
+  //     this.props.test(cart);
+  //   }
+  //   if (!e.target.checked) {
+  //     this.props.test(0);
+  //   }
+  // };
+
+  testProp = e => {
+    if (e.target.checked) {
+      this.props.test(this.props.total);
+    }
+    if (!e.target.checked) {
+      this.props.test(0);
+    }
+  };
   render() {
-    const { itemName, itemImg, itemPrice } = this.props;
+    const { id, itemNum, plusBtn, minusBtn, itemName, itemImg, total } =
+      this.props;
+    const { getCheckboxValue, testProp } = this;
     return (
       <div className="orderItems">
         <div className="orderItemsLeft">
           <div className="checkBox">
-            <input type="checkbox" className="checkHide"></input>
+            <input
+              type="checkbox"
+              className="checkHide"
+              onClick={testProp}
+              value={total}
+            ></input>
           </div>
           <img className="itemImg" src={itemImg} alt="item"></img>
           <div className="itemText">
             <div className="itemName">{itemName}</div>
-            <div className="itemOption">수량: 1개</div>
+            <div className="itemOption">
+              수량:{' '}
+              <button name={id} onClick={minusBtn}>
+                -
+              </button>{' '}
+              {itemNum}개
+              <button name={id} onClick={plusBtn}>
+                +
+              </button>
+            </div>
           </div>
         </div>
         <div className="orderItemsRight">
           <button className="closeBtn"></button>
-          <div className="itemPrice">{itemPrice}</div>
+          <div className="itemPrice">\{total}</div>
         </div>
       </div>
     );

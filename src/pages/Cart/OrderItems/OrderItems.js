1ad67a099c04d2752054942e94e5732b6cf56002
mekemeke94 <thth3194@gmail.com>
2021-08-11 21:03:26 +0900

    Fix: conflict 수정

--- src/pages/Cart/OrderItems/OrderItems.js
+++ src/pages/Cart/OrderItems/OrderItems.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './OrderItems.scss';
 
 class OrderItems extends React.Component {
-<<<<<<< HEAD
   render() {
     const {
       id,
@@ -18,61 +17,10 @@ class OrderItems extends React.Component {
       <div className="orderItems">
         <div className="orderItemsLeft">
           <img className="itemImg" src={itemImg} alt="item" />
-=======
-  constructor() {
-    super();
-
-    this.state = {
-      totalPrice: '',
-      cart: [],
-    };
-  }
-  // getCheckboxValue = e => {
-  //   const { cart } = this.state;
-  //   let totalCart = cart.push(this.props.total);
-  //   console.log('setstate전:', cart);
-  //   this.setState({
-  //     cart: totalCart,
-  //   });
-  //   console.log('setstate 후:', cart);
-  //   if (e.target.checked) {
-  //     this.props.test(cart);
-  //   }
-  //   if (!e.target.checked) {
-  //     this.props.test(0);
-  //   }
-  // };
-
-  testProp = e => {
-    if (e.target.checked) {
-      this.props.test(this.props.total);
-    }
-    if (!e.target.checked) {
-      this.props.test(0);
-    }
-  };
-  render() {
-    const { id, itemNum, plusBtn, minusBtn, itemName, itemImg, total } =
-      this.props;
-    const { getCheckboxValue, testProp } = this;
-    return (
-      <div className="orderItems">
-        <div className="orderItemsLeft">
-          <div className="checkBox">
-            <input
-              type="checkbox"
-              className="checkHide"
-              onClick={testProp}
-              value={total}
-            ></input>
-          </div>
-          <img className="itemImg" src={itemImg} alt="item"></img>
->>>>>>> master
           <div className="itemText">
             <div className="itemName">{itemName}</div>
             <div className="itemOption">
               수량:{' '}
-<<<<<<< HEAD
               <button
                 className="quantityBtn"
                 name={id}
@@ -82,24 +30,13 @@ class OrderItems extends React.Component {
               </button>{' '}
               {itemNum}개
               <button className="quantityBtn" name={id} onClick={plusBtn}>
-=======
-              <button name={id} onClick={minusBtn}>
-                -
-              </button>{' '}
-              {itemNum}개
-              <button name={id} onClick={plusBtn}>
->>>>>>> master
                 +
               </button>
             </div>
           </div>
         </div>
         <div className="orderItemsRight">
-<<<<<<< HEAD
-          <button className="closeBtn" onClick={() => deleteList(id)}></button>
-=======
-          <button className="closeBtn"></button>
->>>>>>> master
+          <button className="closeBtn" onClick={() => deleteList(id)} />
           <div className="itemPrice">\{total}</div>
         </div>
       </div>

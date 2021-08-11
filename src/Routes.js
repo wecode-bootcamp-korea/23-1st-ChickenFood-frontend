29e383489dc1441966feec7aa731b0c4954d285f
JaeHyun <nam2350@gmail.com>
2021-08-11 23:01:16 +0900

    Add: mock data

--- src/Routes.js
+++ src/Routes.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Header from './pages/Header/Header';
 import Main from './pages/Main/Main';
+import Nav from './components/Nav/Nav';
 import Products from './pages/Products/Products';
-import Productdetail from './pages/Products/Productdetail/Productdetail';
 import Footer from './components/Footer/Footer';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
-        {/* <Nav /> */}
+        <Nav />
         <Switch>
-          <Route exact path="/header" component={Header} />
           <Route exact path="/" component={Main} />
           <Route exact path="/products" component={Products} />
-          <Route exact path="/productdetail" component={Productdetail} />
         </Switch>
-        {/* <Footer /> */}
+        <Footer />
       </Router>
     );
   }

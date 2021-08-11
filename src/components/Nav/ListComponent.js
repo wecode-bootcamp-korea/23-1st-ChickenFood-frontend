c45f01a4d03c5adf213f9a5d1e04940ec72ab957
택준 <taekjunsim@gmail.com>
2021-08-11 16:21:42 +0900

    삭제예정 커밋

--- src/components/Nav/ListComponent.js
+++ src/components/Nav/ListComponent.js
@@ -5,7 +5,7 @@ import './nav.scss';
 class ListComponent extends React.Component {
   render() {
     const { path, classname, src, data } = this.props;
-    // console.log(this.props); // history, location 확인
+
     return (
       <li>
         <div

9baa16601063f010db5bea31c35e4e3dc116ce72
Lown Kim <wideplate3@gmail.com>
2021-08-16 15:35:34 +0900

    Add: 최종

--- src/Routes.js
+++ src/Routes.js
@@ -5,7 +5,7 @@ import Main from './pages/Main/Main';
 import SignIn from './pages/Login/SignIn';
 import SignUp from './pages/Login/SignUp';
 import Products from './pages/Products/Products';
-// import Productdetail from './pages/Products/Productdetail/Productdetail';
+import Productdetail from './pages/Products/Productdetail/Productdetail';
 import Cart from './pages/Cart/Cart';
 import BestItem from './pages/Products/BestItem/BestItem';
 import Footer from './components/Footer/Footer';
@@ -19,7 +19,7 @@ class Routes extends React.Component {
           <Route exact path="/" component={Main} />
           <Route exact path="/signin" component={SignIn} />
           <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/products/:pathId" component={Products} />
+          <Route exact path="/productdetail" component={Productdetail} />
           <Route exact path="/bestitem" component={BestItem} />
         </Switch>
         <Footer />
